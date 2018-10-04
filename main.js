482a417205ad0f3049e84743cbe6801253968672
ShawonAshraf <shawon13@live.com>
2018-10-05 03:00:55 +0600

    macOS quit support

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 const { app, BrowserWindow } = require('electron');
 
+let win;
+
 function createWindow() {
-  const win = new BrowserWindow({ width: 800, height: 600});
-  win.loadFile(`${__dirname}/html/index.html`);
+  win = new BrowserWindow({ width: 750, height: 250});
+  win.loadFile(`${__dirname}/static/index.html`);
+
+  // for debugging only
+  // win.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    win = null;
+  });
 }
 
-app.on('ready', createWindow());
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+});
